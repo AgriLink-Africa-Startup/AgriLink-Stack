456278f65422528916f79bf4d2df150c63c4695f
RichardNzembei <richardsonreuben78@gmail.com>
2024-11-28 21:47:14 +0300

    profile setup

--- Frontend/Website/AgriLink/pages/Farmer/profile.vue
+++ Frontend/Website/AgriLink/pages/Farmer/profile.vue
@@ -1,3 +1,17 @@
 <template>
-    <h2>component</h2>
-</template>
+    <div>
+      <AppFarmerProfile/>
+    </div>
+    
+  </template>
+  
+  <script setup>
+
+ 
+  
+  </script>
+  
+  <style scoped>
+  /* Page-specific styling */
+  </style>
+  
